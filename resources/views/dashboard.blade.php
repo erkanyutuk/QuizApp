cf49713e73793a15ec207d59b5b1104c19e07770
Erkan Yutuk <yerkan@std.beu.edu.az>
2021-01-06 01:40:30 +0400

    Jetstream kuruldu node js kuruldu profil foto eklendi

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        Esas Seyfe
+    </x-slot>
+
+    <div class="alert alert-warning">
+                Bootstrap
+            </div>
+           
+</x-app-layout>
